827a7d17705f5ff07803202b5f34f72615062432
Hjr <200293526+g0d0rs4tan101@users.noreply.github.com>
2026-07-25 09:37:41 +1000

    Add files via upload

--- buttons.cs
+++ buttons.cs
@@ -1,24 +1,24 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-07-06 06:31:21.814096600 UTC
+// 2026-07-24 23:36:27.604628400 UTC
 
 namespace CS2Dumper {
     // Module: client.dll
     public static class Buttons {
-        public const nint attack = 0x2065A90;
-        public const nint attack2 = 0x2065B20;
-        public const nint back = 0x2065D60;
-        public const nint duck = 0x2066030;
-        public const nint forward = 0x2065CD0;
-        public const nint jump = 0x2065FA0;
-        public const nint left = 0x2065DF0;
-        public const nint lookatweapon = 0x2355FE0;
-        public const nint reload = 0x2065A00;
-        public const nint right = 0x2065E80;
-        public const nint showscores = 0x2355EC0;
-        public const nint sprint = 0x2065970;
-        public const nint turnleft = 0x2065BB0;
-        public const nint turnright = 0x2065C40;
-        public const nint use = 0x2065F10;
-        public const nint zoom = 0x2355F50;
+        public const nint attack = 0x2094F80;
+        public const nint attack2 = 0x2095010;
+        public const nint back = 0x2095250;
+        public const nint duck = 0x2095520;
+        public const nint forward = 0x20951C0;
+        public const nint jump = 0x2095490;
+        public const nint left = 0x20952E0;
+        public const nint lookatweapon = 0x23BA6B0;
+        public const nint reload = 0x2094EF0;
+        public const nint right = 0x2095370;
+        public const nint showscores = 0x23BA590;
+        public const nint sprint = 0x2094E60;
+        public const nint turnleft = 0x20950A0;
+        public const nint turnright = 0x2095130;
+        public const nint use = 0x2095400;
+        public const nint zoom = 0x23BA620;
     }
 }
--- buttons.zig
+++ buttons.zig
@@ -1,24 +1,24 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-07-06 06:31:21.814096600 UTC
+// 2026-07-24 23:36:27.604628400 UTC
 
 pub const cs2_dumper = struct {
     // Module: client.dll
     pub const buttons = struct {
-        pub const attack: usize = 0x2065A90;
-        pub const attack2: usize = 0x2065B20;
-        pub const back: usize = 0x2065D60;
-        pub const duck: usize = 0x2066030;
-        pub const forward: usize = 0x2065CD0;
-        pub const jump: usize = 0x2065FA0;
-        pub const left: usize = 0x2065DF0;
-        pub const lookatweapon: usize = 0x2355FE0;
-        pub const reload: usize = 0x2065A00;
-        pub const right: usize = 0x2065E80;
-        pub const showscores: usize = 0x2355EC0;
-        pub const sprint: usize = 0x2065970;
-        pub const turnleft: usize = 0x2065BB0;
-        pub const turnright: usize = 0x2065C40;
-        pub const use: usize = 0x2065F10;
-        pub const zoom: usize = 0x2355F50;
+        pub const attack: usize = 0x2094F80;
+        pub const attack2: usize = 0x2095010;
+        pub const back: usize = 0x2095250;
+        pub const duck: usize = 0x2095520;
+        pub const forward: usize = 0x20951C0;
+        pub const jump: usize = 0x2095490;
+        pub const left: usize = 0x20952E0;
+        pub const lookatweapon: usize = 0x23BA6B0;
+        pub const reload: usize = 0x2094EF0;
+        pub const right: usize = 0x2095370;
+        pub const showscores: usize = 0x23BA590;
+        pub const sprint: usize = 0x2094E60;
+        pub const turnleft: usize = 0x20950A0;
+        pub const turnright: usize = 0x2095130;
+        pub const use: usize = 0x2095400;
+        pub const zoom: usize = 0x23BA620;
     };
 };
